a17e8ae4a1ac9a2fcc8c7433a20ab6e5675098ba
Alexander Ruben Mamani <labm2014@gmail.com>
2023-07-11 21:13:27 -0300

    creando el comannd de roles

---
+++ src/Identity.Api/Controllers/RoleController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : BaseController
+    {
+        public RoleController(IMediator mediator) : base(mediator)
+        {
+
+        }
+        [HttpPost("Rol")]
+        public async Task<IActionResult> InsertRole([FromBody] InsertRoleCommand command)
+        => Ok(await Mediator.Send(command));
+    }
+}
---
+++ src/Identity.Dto/Request/Command/InsertRoleCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.Dto.Request.Command
+{
+    internal class InsertRoleCommand
+    {
+        public RoleId { get; set; }
+    }
+}
